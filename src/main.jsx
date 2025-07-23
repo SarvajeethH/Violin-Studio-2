236cc1722e207c02dab3b70685dbf1c277b7f362
SarvajeethH <168371529+SarvajeethH@users.noreply.github.com>
2025-07-23 17:24:41 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css' // This line is added to import the styles
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+// This line is essential for loading your styles
+import './App.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
